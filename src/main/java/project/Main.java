02e5778dc39dc89927de6e7065275a7b829b3ff0
Emad Ali <mc.lilzee@hotmail.com>
2022-10-18 07:13:02 +0200

    Add company creation

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -4,12 +4,19 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
+
+    private static CompaniesController controller;
+    private static Statement statement;
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("org.h2.Driver");
         Connection conn = DriverManager.getConnection("jdbc:h2:./database/carsharing");
         conn.setAutoCommit(true);
-        Statement st = conn.createStatement();
-        st.executeUpdate("CREATE TABLE IF NOT EXISTS COMPANY (ID INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR NOT NULL)");
+        statement = conn.createStatement();
+
+        controller = new CompaniesController();
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS company (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR NOT NULL)");
+        menuOptions();
         conn.close();
     }
 
@@ -40,6 +47,40 @@ public class Main {
     }
 
     private static void logInAsManager(Scanner scanner) {
+        boolean quit = false;
+        while (!quit) {
+            switch (scanner.nextLine()) {
+                case "1":
+                    System.out.println(controller.toString());
+                    break;
+                case "2":
+                    createCompany(scanner);
+                    break;
+                case "0":
+                    quit = true;
+                    break;
+                default:
+                    System.out.println("Wrong input, please choose again:");
+                    printLogInAsManagerInstructions();
+            }
+        }
+    }
+
+    private static void printLogInAsManagerInstructions() {
+        System.out.println("1. Company List");
+        System.out.println("2. Create a company");
+        System.out.println("0. Back");
+    }
+
+    private static void createCompany(Scanner scanner) {
+        System.out.println("Enter the company name:");
+        String input = scanner.nextLine();
+
+        try {
+            statement.executeUpdate("INSERT INTO COMPANY (name) VALUES (%s)", input.split(""));
+        } catch (SQLException e) {
+            System.out.println("Failed to add new company");
+        }
 
     }
 }
