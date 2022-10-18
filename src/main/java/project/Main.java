8db33d8b5c78f0de828e4fe921e4bcecc95357fc
Emad Ali <mc.lilzee@hotmail.com>
2022-10-18 06:41:43 +0200

    Create menu options

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,14 +1,45 @@
 package project;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-            Class.forName ("org.h2.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:h2:./database/carsharing");
-            conn.setAutoCommit(true);
-            Statement st = conn.createStatement();
-            st.executeUpdate("CREATE TABLE IF NOT EXISTS COMPANY (ID INT, NAME VARCHAR)");
-            conn.close();
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:./database/carsharing");
+        conn.setAutoCommit(true);
+        Statement st = conn.createStatement();
+        st.executeUpdate("CREATE TABLE IF NOT EXISTS COMPANY (ID INT PRIMARY KEY AUTO_INCREMENT, NAME VARCHAR NOT NULL)");
+        conn.close();
+    }
+
+    private static void menuOptions() {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        while (!quit) {
+            switch (scanner.nextLine()) {
+                case "1":
+                    logInAsManager(scanner);
+                    break;
+                case "0":
+                    quit = true;
+                    break;
+                default:
+                    System.out.println("Incorrect option chosen :");
+                    printMenuOptionsInstructions();
+                    break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void printMenuOptionsInstructions() {
+        System.out.println("1. Log in as a manager");
+        System.out.println("0. Exit");
+    }
+
+    private static void logInAsManager(Scanner scanner) {
+
     }
 }
