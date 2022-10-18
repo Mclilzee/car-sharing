daca818c98030585dc2548bb05dd4fec131502b7
Emad Ali <mc.lilzee@hotmail.com>
2022-10-18 06:34:42 +0200

    Connect to h2 database

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -1,7 +1,14 @@
 package project;
 
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+            Class.forName ("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:./database/carsharing");
+            conn.setAutoCommit(true);
+            Statement st = conn.createStatement();
+            st.executeUpdate("CREATE TABLE IF NOT EXISTS COMPANY (ID INT, NAME VARCHAR)");
+            conn.close();
     }
 }
